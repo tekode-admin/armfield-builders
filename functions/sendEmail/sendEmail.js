f5908d0e742d4c706044e62368bcafbe950585f7
Matt Armfield <devmatt3@gmail.com>
2023-04-22 21:57:48 -0400

    Add in netlify cli and start building sendMail function

---
+++ functions/sendEmail/sendEmail.js
@@ -0,0 +1,19 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.sendgrid)
+const msg = {
+  to: 'admin@tekode.com', // Change to your recipient
+  from: 'no-reply@tekode.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
--- gatsby-config.js
+++ gatsby-config.js
@@ -79,5 +79,21 @@ module.exports = {
         fonts: [`Iceland`, `Syne`],
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-L625L5GQ0K", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // gtagConfig: {
+        //   optimize_id: "GTM-WVX7QML",
+        //   anonymize_ip: true,
+        //   cookie_expires: 0,
+        // },
+      },
+    },
   ],
 }
